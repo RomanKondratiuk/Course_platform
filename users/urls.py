c5ee53657de04c00a8d90cb116678057b926f308
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2024-02-07 21:16:09 -0500

    Configured the use of JWT authorization

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
 from users.views import PaymentsListApiView, UserListApiView
@@ -10,4 +11,6 @@ urlpatterns = [
     path('payments/', PaymentsListApiView.as_view(), name='payments_list'),
     path('user_list/', UserListApiView.as_view(), name='user_list'),
 
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
